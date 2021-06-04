53e0658717fae2d332e1bf89c3c6c666be1a3121
Bomie <madmagican3@gmail.com>
2021-06-04 09:27:32 +0100

    Changed the system to be based off initial catalogs downloaded

--- NugetInvestigation/Program.cs
+++ NugetInvestigation/Program.cs
@@ -2,26 +2,17 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NugetInvestigation.Models;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace NugetInvestigation
 {
     class Program
     {
         public static string WorkingDir = "C:\\Users\\madma\\Desktop\\Nuget working dir";
-
-        static int GetLastFileNo(string[] getLastFileNo)
-        {
-            int highestNo = -1;
-            foreach (var t in getLastFileNo)
-            {
-                var fileName = t.Split('\\').Last().Replace(".json", "");
-                highestNo = Math.Max(highestNo, int.Parse(fileName));
-            }
-
-            return highestNo + 1;
-        }
 
         static void Main(string[] args)
         {
@@ -31,6 +22,7 @@ namespace NugetInvestigation
             //setup the folders required
             var resultFolder = $"{WorkingDir}\\Results";
             var stopFolder = $"{WorkingDir}\\insertFilesHereToStopAfterLatestRun";
+            var catalogs = $"{WorkingDir}\\catalogs";
 
             //delete the stop folder, as we dont know if it's used and we want it empty
             try
@@ -44,94 +36,51 @@ namespace NugetInvestigation
             //create the working dirs if they're not working
             Directory.CreateDirectory(stopFolder);
             Directory.CreateDirectory(resultFolder);
+            Directory.CreateDirectory(catalogs);
 
-            //setup the start of the redo functionality
-            Console.WriteLine($"Handling redos");
-            var taskListRedo = new List<Task>();
-            var lastNum = int.Parse(Directory.GetFiles(resultFolder).Last().Replace(".json", ""));
-            var numbersToDo = new List<int>();
-            for (int i = 0; i < lastNum; i++)
+            var hasHitEndOfCatalogs = false;
+            var counter = 0;
+            //setup the default details
+            var client = new HttpClient();
+
+            if (!Directory.GetFiles(catalogs).Any())
             {
-                if (!Directory.GetFiles(resultFolder).Any(x => x == $"{i}.json"))
+                do
                 {
-                    numbersToDo.Add(i);
-                }
+                    var result = client.GetAsync($"https://api.nuget.org/v3/catalog0/page{counter}.json").Result;
+                    if (!result.IsSuccessStatusCode) //Once it hits a 404 we're done
+                    {
+                        hasHitEndOfCatalogs = true;
+                    }
+
+                    var content = result.Content.ReadAsStringAsync().Result;
+                    var catalogPage = JsonConvert.DeserializeObject<CatalogPage>(content);
+                    File.WriteAllText($"{catalogs}\\{counter}.json", JsonSerializer.Serialize(catalogPage));
+                    Console.WriteLine($"Got catalog {counter} and continuing");
+                    counter += 1;
+                } while (!hasHitEndOfCatalogs);
             }
 
-            foreach (var id in numbersToDo)
+            //get all the ints currently in the result list
+            var resultList = Directory.GetFiles(resultFolder).Select(x => x.Split('\\').Last().Replace(".json", ""));
+            var catalogList = Directory.GetFiles(catalogs).Select(x => x.Split('\\').Last().Replace(".json", ""));
+
+            var notDoneList = new List<int>();
+
+            foreach (var catalog in catalogList)
             {
-                //work out the archive details
-                var nugetArchiveFolder = $"{WorkingDir}\\Archive{id}";
-                var nugetDownloadFolder = $"{WorkingDir}\\Initial{id}";
-
-                //try and remove them if they exist, dont try and reuse existing elements, we dont know what bits failed
-                try
+                if (!resultList.Contains(catalog))
                 {
-                    Directory.Delete(nugetArchiveFolder, true);
-                }
-                catch (Exception ex)
-                {
-                }
-
-                try
-                {
-                    Directory.Delete(nugetDownloadFolder, true);
-                }
-                catch (Exception ex)
-                {
-                }
-
-                //start the task of handling the catalog
-                var task = Task.Run(async () =>
-                    await nugetHandler.MainMethod(nugetDownloadFolder, nugetArchiveFolder, resultFolder, id));
-                //add it to the task list
-                taskListRedo.Add(task);
-                if (taskListRedo.Count % 20 == 0)
-                {
-                    try
-                    {
-                        //wait for all the tasks to be done
-                        Task.WaitAll(taskListRedo.ToArray());
-
-                        //clear the task list
-                        taskListRedo.Clear();
-
-                        //if we've got a file in the stop folder then stop running the code
-                        if (Directory.GetFiles(stopFolder).Any())
-                        {
-                            break;
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        //track the errors in a anon object
-                        Directory.CreateDirectory($"{WorkingDir}\\Errors");
-                        File.WriteAllText($"{WorkingDir}\\Errors\\Errors{id}.txt", JsonSerializer.Serialize(new
-                        {
-                            message = ex.Message,
-                            stacktrace = ex.StackTrace,
-                            innerStack = ex.InnerException
-                        }));
-
-                        //if we've got a file in the stop folder then stop running the code
-                        if (Directory.GetFiles(stopFolder).Any())
-                        {
-                            break;
-                        }
-                    }
+                    notDoneList.Add(int.Parse(catalog));
                 }
             }
-
 
             var taskList = new List<Task>();
 
-            //get the latest files from the results
-            var files = Directory.GetFiles(resultFolder);
-
-            for (int i = GetLastFileNo(files);; i++)
+            foreach (var catalog in notDoneList)
             {
                 //get the id, and set it because for the task it will be a pointer
-                var id = i;
+                var id = catalog;
 
                 //setup the working folders
                 var nugetArchiveFolder = $"{WorkingDir}\\Archive{id}";
